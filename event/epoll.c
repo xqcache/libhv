ddba07e5683cf6aedbef709b3454537d7efe7dc8
hicache <hicache@outlook.com>
2023-10-14 13:18:18 +0800

    修复msvc编译失败的问题

--- event/epoll.c
+++ event/epoll.c
@@ -13,19 +13,19 @@
 #endif
 
 #include "array.h"
-#define EVENTS_INIT_SIZE    64
+#define EVENTS_INIT_SIZE 64
 ARRAY_DECL(struct epoll_event, events);
 
 typedef struct epoll_ctx_s {
-    int                 epfd;
-    struct events       events;
+    int epfd;
+    struct events events;
 } epoll_ctx_t;
 
 int iowatcher_init(hloop_t* loop) {
     if (loop->iowatcher) return 0;
     epoll_ctx_t* epoll_ctx;
     HV_ALLOC_SIZEOF(epoll_ctx);
-    epoll_ctx->epfd = epoll_create(EVENTS_INIT_SIZE);
+    epoll_ctx->epfd = (int)epoll_create(EVENTS_INIT_SIZE);
     events_init(&epoll_ctx->events, EVENTS_INIT_SIZE);
     loop->iowatcher = epoll_ctx;
     return 0;
@@ -34,7 +34,7 @@ int iowatcher_init(hloop_t* loop) {
 int iowatcher_cleanup(hloop_t* loop) {
     if (loop->iowatcher == NULL) return 0;
     epoll_ctx_t* epoll_ctx = (epoll_ctx_t*)loop->iowatcher;
-    epoll_close(epoll_ctx->epfd);
+    epoll_close((HANDLE)epoll_ctx->epfd);
     events_cleanup(&epoll_ctx->events);
     HV_FREE(loop->iowatcher);
     return 0;
@@ -65,7 +65,7 @@ int iowatcher_add_event(hloop_t* loop, int fd, int events) {
         ee.events |= EPOLLOUT;
     }
     int op = io->events == 0 ? EPOLL_CTL_ADD : EPOLL_CTL_MOD;
-    epoll_ctl(epoll_ctx->epfd, op, fd, &ee);
+    epoll_ctl((HANDLE)epoll_ctx->epfd, op, fd, &ee);
     if (op == EPOLL_CTL_ADD) {
         if (epoll_ctx->events.size == epoll_ctx->events.maxsize) {
             events_double_resize(&epoll_ctx->events);
@@ -98,7 +98,7 @@ int iowatcher_del_event(hloop_t* loop, int fd, int events) {
         ee.events &= ~EPOLLOUT;
     }
     int op = ee.events == 0 ? EPOLL_CTL_DEL : EPOLL_CTL_MOD;
-    epoll_ctl(epoll_ctx->epfd, op, fd, &ee);
+    epoll_ctl((HANDLE)epoll_ctx->epfd, op, fd, &ee);
     if (op == EPOLL_CTL_DEL) {
         epoll_ctx->events.size--;
     }
@@ -107,9 +107,9 @@ int iowatcher_del_event(hloop_t* loop, int fd, int events) {
 
 int iowatcher_poll_events(hloop_t* loop, int timeout) {
     epoll_ctx_t* epoll_ctx = (epoll_ctx_t*)loop->iowatcher;
-    if (epoll_ctx == NULL)  return 0;
+    if (epoll_ctx == NULL) return 0;
     if (epoll_ctx->events.size == 0) return 0;
-    int nepoll = epoll_wait(epoll_ctx->epfd, epoll_ctx->events.ptr, epoll_ctx->events.size, timeout);
+    int nepoll = epoll_wait((HANDLE)epoll_ctx->epfd, epoll_ctx->events.ptr, epoll_ctx->events.size, timeout);
     if (nepoll < 0) {
         if (errno == EINTR) {
             return 0;
